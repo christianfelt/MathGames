a3bd709c4a3ef39391b3b52ec8bd7831826487bb
christianfelt <christianfelt@comcast.net>
2019-12-16 10:02:23 -0700

    Card class added, deck.py simplified, dealer final turns allowed.

--- blackjack.py
+++ blackjack.py
@@ -3,8 +3,9 @@
 
 import deck
 
+
 class Blackjack:
-    def __init__(self, max_score = 21):
+    def __init__(self, max_score=21):
         self.player_score = 0
         self.dealer_score = 0
         self.games_played = 0
@@ -12,7 +13,7 @@ class Blackjack:
         self.games_lost = 0
         self.games_drawn = 0
         self.player_name = "Anonymous"
-        self.current_deck = deck.Deck()
+        self.deck = deck.Deck()
         self.max_score = max_score
         self.dealer_max_score = 14
 
@@ -20,33 +21,33 @@ class Blackjack:
         self.player_name = name
 
     def do_dealer_turn(self):
-        this_card = self.current_deck.draw()
-        this_score = self.current_deck.get_card_value(this_card)
+        this_card = self.deck.draw()
+        this_score = this_card.value
         self.dealer_score += this_score
-        print("The dealer drew", this_card)
+        print("The dealer drew the", this_card.name, "of", this_card.suit)
         print("The dealer's score is", self.dealer_score)
 
     def do_player_turn(self):
-        this_card = self.current_deck.draw()
-        this_score = self.current_deck.get_card_value(this_card)
+        this_card = self.deck.draw()
+        this_score = this_card.value
         self.player_score += this_score
-        print("You drew", this_card)
+        print("You drew the", this_card.name, "of", this_card.suit)
         print("Your score is", self.player_score)
-
 
 
 if __name__ == '__main__':
     blackjack = Blackjack()
+    blackjack.deck.shuffle()
     name = input("What is your name? ")
     blackjack.set_player_name(name)
     print("Hello,", blackjack.player_name)
-    first_card = blackjack.current_deck.draw()
-    print("Your first card is", first_card)
-    first_score = blackjack.current_deck.get_card_value(first_card)
+    first_card = blackjack.deck.draw()
+    print("Your first card is the", first_card.name, "of", first_card.suit)
+    first_score = first_card.value
     blackjack.player_score += first_score
-    game_in_progress = True
+    print("Your score is", blackjack.player_score)
     blackjack.do_dealer_turn()
-    while game_in_progress:
+    while True:
         response = input("Would you like to draw another card? (y/n) ")
         while response.lower() == "" or response.lower() not in "yn":
             response = input("Input not recognized. Please enter 'y' or 'n'. ")
@@ -54,14 +55,17 @@ if __name__ == '__main__':
             blackjack.do_player_turn()
             if blackjack.player_score > blackjack.max_score:
                 print("Your score exceeds the max score. Therefore, you lose.")
-                game_in_progress = False
+                break
             if blackjack.dealer_score < blackjack.dealer_max_score:
                 blackjack.do_dealer_turn()
                 if blackjack.dealer_score > blackjack.max_score:
                     print("The dealer's score exceeds the max score. Therefore, you win.")
-                    game_in_progress = False
+                    break
+            else:
+                print("The dealer did not draw a card. The dealer's score is still", blackjack.dealer_score)
         elif response.lower() == "n":
-            game_in_progress = False
+            while blackjack.dealer_score < blackjack.dealer_max_score:
+                blackjack.do_dealer_turn()
             print("Your final score is", blackjack.player_score)
             print("The dealer's final score is", blackjack.dealer_score)
             if blackjack.player_score > blackjack.dealer_score:
@@ -73,9 +77,7 @@ if __name__ == '__main__':
             else:
                 print("The game was a draw.")
                 blackjack.games_drawn += 1
+            break
         else:
             print("Did you enter 'yn'? You loser. You lose.")
-            game_in_progress = False
     blackjack.games_played += 1
-
-
---
+++ card.py
@@ -0,0 +1,5 @@
+class Card:
+    def __init__(self, name, suit, value):
+        self.name = name
+        self.suit = suit
+        self.value = value
