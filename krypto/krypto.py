79820471642a730d7047e15eb529ff6a59234a80
christianfelt <christianfelt@comcast.net>
2019-12-17 12:46:07 -0700

    Krypto first draft with timer done.

--- krypto/krypto.py
+++ krypto/krypto.py
@@ -5,36 +5,39 @@
 # TODO: Implement solution checking
 # TODO: Implement computer opponent who finds correct solution after handicap time (random deviation around
 #  an average, selected according to player's chosen difficulty level.)
+# TODO: Implement custom parser to catch all potential rule breaking.
+# TODO: Implement saving high scores with username between runs
 
 import random
 import re
-import operator
+import threading
+import os
 
 
 class Krypto:
     """A popular math game."""
 
-    operator_table = {
-        """All operators allowed in the game. Map from strings to operators."""
-        '+': operator.add,
-        '-': operator.sub,
-        '*': operator.mul,
-        '/': operator.truediv
-    }
+    rules = "\nFind a way to add, subtract, multiply, or divide the numbers on the following playing cards to produce\n " \
+            "the number on the target card. Each playing card must be used exactly once. In your solution,\n " \
+            "only the numbers on the playing cards and the symbols +, -, *, /, and parentheses are allowed.\n "
 
-    rules = "Find a way to add, subtract, multiply, or divide (without remainder) the numbers on the following " \
-            "playing\n cards to produce the number on the target card. Each playing card must be used exactly once. " \
-            "When\n evaluating your solution, all arithmetic operations will be performed from left to right. In your " \
-            "solution, \nonly the numbers on the playing cards and the symbols +, -, *, and / are allowed. No " \
-            "negative numbers \n are allowed. "
+    allowed_characters = ['+', '-', '/', '*', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '(', ')']
 
-    def __init__(self):
+    def __init__(self, time_limit=60):
         """Fill and shuffle deck and initialize game stats."""
         self.deck = []
         self.populate_deck()
         self.games_won = 0
         self.games_lost = 0
+        self.timer = threading.Timer(time_limit, self.time_up)
 
+    def time_up(self):
+        print("Time's up!")
+        print("Games won:", self.games_won)
+        print("Games lost:", self.games_lost)
+        total_score = self.games_won - self.games_lost
+        print("Total score:", total_score)
+        os._exit(0)
 
     def populate_deck(self):
         """Fill deck with standard Krypto card values and shuffle."""
@@ -75,10 +78,43 @@ class Krypto:
             self.populate_deck()
             return self.deck.pop(0)
 
+    def play_krypto(self):
+        self.timer.start()
+        print(Krypto.rules)
+        while True:
+            print("\nPlaying Cards:")
+            playing_cards = []
+            for i in range(0, 5):
+                playing_cards.append(self.draw())
+                print(playing_cards[-1], end=" ")
+            print("\nTarget Card:")
+            target_card = self.draw()
+            print(target_card)
+            answer = input("Do you have a solution?\n")
+            evaluation = Krypto.evaluate_answer(answer, playing_cards, target_card)
+            print(evaluation)
+            if evaluation == "Correct!":
+                self.games_won += 1
+            else:
+                self.games_lost += 1
+            print("Games won:", self.games_won)
+            print("Games lost:", self.games_lost)
+
     @staticmethod
     def evaluate_answer(answer_string, playing_cards, target_card):
         """Check if answer is correct."""
-        answer_string = answer_string.replace(" ", "") # Get rid of spaces.
+        answer_string = answer_string.replace(" ", "")  # Get rid of spaces.
+        if len(answer_string) > len(playing_cards) + 20: # Some basic security checks on input to eval()
+            return "Solution is too long."
+        for char in answer_string:
+            if char not in Krypto.allowed_characters:
+                return "Solution contains invalid characters."
+        try:
+            result = eval(answer_string)
+        except SyntaxError:
+            return "Solution has incorrect syntax."
+        answer_string = answer_string.replace("(", "")
+        answer_string = answer_string.replace(")", "")
         numbers = re.split('\+|-|\*|/', answer_string)
         if len(numbers) != len(playing_cards):
             return "Solution contains wrong number of numbers."
@@ -86,46 +122,19 @@ class Krypto:
         operators = operators[1:-1] # Remove "" at beginning and end of list.
         if len(operators) + 1 != len(numbers):
             return "Solution uses wrong number of operators"
-        result = int(numbers[0])
-        for i in range(0, len(operators)):
-            try:
-                intermediate_result = Krypto.operator_table[operators[i]](result, int(numbers[i+1]))
-            except KeyError:
-                return "Solution uses invalid operators."
-            if intermediate_result < 0:
-                return "Negative numbers are not allowed."
-            if intermediate_result % 1 != 0:
-                return "Division with remainders is not allowed."
-            result = intermediate_result
         for card in playing_cards:
-            if card not in numbers:
+            if str(card) not in numbers:
                 return "Solution does not use all the playing cards."
+        for number in numbers:
+            if int(number) not in playing_cards:
+                return "Solution uses numbers not in playing cards."
         if result == target_card:
             return "Correct!"
         else:
-            return "Incorrect."
+            return "= " + str(result) + ". Incorrect."
 
 
 if __name__ == '__main__':
     krypto = Krypto()
-    print(Krypto.rules)
-    keep_playing = True
-    while keep_playing:
-        print("Playing Cards:")
-        playing_cards = []
-        for i in range(0, 5):
-            playing_cards.append(krypto.draw())
-            print(playing_cards[-1], end=" ")
-        print("\nTarget Card:")
-        target_card = krypto.draw()
-        print(target_card)
-        answer = input("Do you have a solution?\n")
-        evaluation = Krypto.evaluate_answer(answer, playing_cards, target_card)
-        print(evaluation)
-        if evaluation == "Correct!":
-            krypto.games_won += 1
-        elif evaluation == "Incorrect.":
-            krypto.games_lost += 1
-        print("Games won:", krypto.games_won)
-        print("Games lost:", krypto.games_lost)
-        keep_playing = input("Would you like to keep playing? (y/n) ").lower() == "y"
+    krypto.play_krypto()
+
